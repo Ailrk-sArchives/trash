adb4128a5792745139102231a4f776f51b49e119
Jimmy <jimmy123good@hotmail.com>
2020-06-20 19:07:48 -0700

    tmp merge sort

--- A-tour-to-cpp/tp.cc
+++ A-tour-to-cpp/tp.cc
@@ -1,6 +1,10 @@
 #include <iostream>
 
 namespace tpgcd {
+/* gcd :: Int -> Int -> Int
+ * gcd a 0 = a
+ * gcd a b = gcd b $ a % b
+ */
 template <int a, int b> struct gcd {
   static constexpr int value = gcd<b, a % b>::value;
 };
@@ -10,6 +14,11 @@ void run() { std::cout << "GCD: " << gcd<25, 50>::value << std::endl; }
 } // namespace tpgcd
 
 namespace tpfib {
+/* fib :: Int -> Int
+ * fib 0 = 1
+ * fib 1 = 1
+ * fib i = (fib $ i - 1) $ (fib $ i - 2)
+ */
 template <int i> struct fib {
   static constexpr int value = fib<i - 1>::value + fib<i - 2>::value;
 };
@@ -20,6 +29,11 @@ void run() { std::cout << "FIB: " << fib<5>::value << std::endl; }
 } // namespace tpfib
 
 namespace tpif {
+/* data Union a b = a | b
+ * if :: Bool -> a -> b -> Union a b
+ * if false a b = b
+ * if true a b = a
+ */
 template <bool C, typename Then, typename Else> struct If {};
 template <typename Then, typename Else> struct If<false, Then, Else> {
   using test = Else;
@@ -37,11 +51,11 @@ struct B {
 };
 
 struct C {
-  static inline void exec() { std::cout << "yikes"; }
+  static inline void exec() { std::cout << "yikes" << std::endl; }
 };
 
 struct D {
-  static inline void exec() { std::cout << "yummies"; }
+  static inline void exec() { std::cout << "yummies" << std::endl; }
 };
 
 auto run() -> void {
@@ -52,28 +66,46 @@ auto run() -> void {
 } // namespace tpif
 
 namespace tplist {
+/* data List = Nil | Cons a (List a)
+ */
 struct Nil {};
-
 template <int a, typename L> struct List {
   static constexpr int head = a;
   using tail = L;
 };
 
-using mylist = List<'a', List<'b', List<'c', Nil>>>;
+using mylist = List<10, List<100, List<2, Nil>>>;
 
-template <typename L> struct sum {
-  static constexpr int value = 0;
-};
+/* sum :: (Semiring a) => List a -> a
+ * sum [] = 0
+ * sum (x:xs) = x + (sum xs)
+ */
+template <typename...> struct sum { static constexpr int value = 0; };
 template <int a, typename Tail> struct sum<List<a, Tail>> {
   static constexpr int value = a + sum<Tail>::value;
 };
 
-}
+/* foldr
+ */
+template <typename L, template <int> typename F, template <int, int> typename R,
+          int Base>
+struct foldr {
+  static constexpr int value = Base;
+};
+template <int a, typename Tail, template <int> typename F,
+          template <int, int> typename R, int Base>
+struct foldr<List<a, Tail>, F, R, Base> {
+  static constexpr int value =
+      R<F<a>::value, foldr<Tail, F, R, Base>::value>::value;
+};
+
+} // namespace tplist
 
 int main(void) {
   std::cout << tpgcd::gcd<28, 100>::value << std::endl;
   std::cout << tpfib::fib<10>::value << std::endl;
   tpif::run();
+  std::cout << "sum: " << tplist::sum<tplist::mylist>::value << std::endl;
 
   return 0;
 }
---
+++ A-tour-to-cpp/tpmergesort.cc
@@ -0,0 +1,88 @@
+
+struct Nil {};
+
+template <int a, typename L> struct List {
+  static constexpr int head = a;
+  using tail = L;
+};
+
+template <typename A, typename B> struct Pair {
+  using first = A;
+  using second = B;
+};
+
+template <bool C, typename Then, typename Else> struct if_ {};
+template <typename Then, typename Else> struct if_<false, Then, Else> {
+  using test = Then;
+};
+template <typename Then, typename Else> struct if_<true, Then, Else> {
+  using test = Else;
+};
+
+template <typename L> struct Split {};
+template <> struct Split<Nil> { using type = Pair<Nil, Nil>; };
+template <int a> struct Split<List<a, Nil>> {
+  using type = Pair<List<a, Nil>, Nil>;
+};
+template <int a, int b, typename Tail> struct Split<List<a, List<b, Tail>>> {
+private:
+  using _Split_rec = typename Split<Tail>::type;
+
+public:
+  using type = Pair<List<a, typename _Split_rec::first>,
+                    List<b, typename _Split_rec::second>>;
+};
+
+struct A {
+  static constexpr int result = 1;
+};
+
+struct B {
+  static constexpr int result = 0;
+};
+
+// merge
+template <template <int, int> typename P, typename L1, typename L2>
+struct Merge {};
+
+template <template <int, int> typename P, typename L2>
+struct Merge<P, Nil, L2> {
+  using type = L2;
+};
+
+template <template <int, int> typename P, typename L1>
+struct Merge<P, L1, Nil> {
+  using type = L1;
+};
+
+template <template <int, int> typename P, int A1, typename Tail1, int A2,
+          typename Tail2>
+struct Merge<P, List<A1, Tail1>, List<A2, Tail2>> {
+  using type = typename if_<
+      P<A1, A2>::value,
+      List<A1, typename Merge<P, Tail1, List<A2, Tail2>>::type>,
+      List<A2, typename Merge<P, List<A2, Tail1>, Tail2>::type>>::test;
+};
+
+// sort
+template <template <int, int> typename P, typename L>
+struct Sort {
+private:
+  using _split_list = typename Split<L>::type;
+  using _l1 = Sort<P, typename _split_list::first>;
+  using _l2 = Sort<P, typename _split_list::second>;
+
+public:
+  using type = typename Merge<P, _l1, _l2>::type;
+};
+
+template <template <int, int> typename P>
+struct Sort<P, Nil> {
+  using type = Nil;
+};
+
+template <template <int, int> typename P, int a>
+struct Sort<P, List<a, Nil>> {
+  using type = List<a, Nil>;
+};
+
