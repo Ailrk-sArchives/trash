51b4596ea6b870da8144046e710b7bdf730de17c
Jimmy <jimmy123good@hotmail.com>
2020-09-16 06:52:06 -0700

    new

---
+++ sklearn-samples/clustering/kmeans-assumptions.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+
+plt.figure(figsize=(12, 12))
+
+n_samples = 1500
+random_state = 170
+X, y = make_blobs(n_samples=n_samples, random_state=random_state)
+
+# incorrect number of clusters
+y_pred = KMeans(n_clusters=2, random_state=random_state).fit_predict(X)
+
+
