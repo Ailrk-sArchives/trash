195e4d45863c3684fb02c55bfd9162bfed6558d6
Jimmy <jimmy123good@hotmail.com>
2021-06-04 21:56:23 -0700

    gen mon

--- haskell/src/Cat/Monads1.hs
+++ haskell/src/Cat/Monads1.hs
@@ -11,6 +11,14 @@ import           Data.Maybe
 
 data Position = Pos { col :: Int, line :: Int } deriving (Eq, Show)
 
+-- We are describing effect with functons and data types only, it's clear to
+-- see where the verbosity of monad appears.
+--
+-- The most annoying part is you always need to lift an operation to the right
+-- level of the monad stack.
+--
+-- Typeclass already removed a lot of boilerplates, but there are still a lot of
+-- extra code needs to be written.
 
 -- starts from the identity monad.
 -- We can gradually adding more functionalities.
@@ -18,6 +26,7 @@ data Position = Pos { col :: Int, line :: Int } deriving (Eq, Show)
 -- I monad: Identify --------------------------
 data I a = I a
 
+-- Monad Instance --
 unitI :: a -> I a
 unitI = I
 
@@ -26,54 +35,150 @@ bindI (I a) f =  f a
 ------------------------------------------------
 
 -- E monad: Error ------------------------------
-data E a = Success a | Error String
+data E a = Success a | Error String deriving (Eq, Show)
 
-errorE s = Error s
 
+-- Monad Instance --
 unitE a = Success a
-
 (Success a) `bindE` k = k a
 (Error e) `bindE` _   = Error e
 
-showE (Success a) = "Success: " ++ showval a
+errorE s = Error s
+
+showE (Success a) = "Success: " ++ show a
 showE (Error e)   = "Error: " ++ e
 ------------------------------------------------
 
 
 -- PE monad: Position + Error  -----------------
-newtype P a = P (Position -> E a)
+newtype PE a = PE { unPE :: (Position -> E a) }
 
+-- Monad Instance --
+unitPE a = PE $ \_ -> unitE a
 
-unitP = P . const unitE
-m `bindP` k = P $ \p -> m p `bindE` (\x -> k x p)
+bindPE :: PE a -> (a -> PE b) -> PE b
+m `bindPE` k = PE $ \p ->
+  (unPE m) p `bindE` (\x ->
+    (unPE (k x)) p)
 
--- lift error.
-errorP s = P $ \p -> errorE (show p ++ ": " ++ s)
+-- Position.
+-- | reset position
+resetPE :: Position -> PE x -> PE x
+resetPE pos (PE m) = PE (\p -> m pos)
 
--- undate position.
-resetP :: Position -> P x -> P x
-resetP pos (P m) = P (\p -> m pos)
-
+errorPE s = PE $ \p -> errorE (s ++ ", Position:" ++ show p)
 ------------------------------------------------
 
 
--- PES monad: Position + Error + State Transformer
-newtype S s a = S (s -> (a, s))
 
-unitS a = S $ \s -> (a, s)
-m `bindS` k = \s -> let (a, s') = m s
-                        (b, s'') = k a s'
-                     in (b, s'')
+-- SPE monad: Position + Error + State Transformer
+newtype SPE s a = SPE { unSPE :: (s -> PE (a, s)) }
 
-tickS :: S s ()
-tickS = undefined
+unitSPE a = SPE $ \s -> unitPE (a, s)
+
+bindSPE :: SPE s a -> (a -> SPE s b) -> SPE s b
+m `bindSPE` k = SPE $ \s ->
+  (unSPE m $ s) `bindPE` (\(a, s') ->
+    (unSPE (k a) $ s') `bindPE` (\(b, s'') ->
+      unitPE (b, s'')))
+
+-- test1 (seems work)
+t1 = (unPE (unSPE t "initial state")) (Pos 0 0)
+  where
+    t = (unitSPE 1) `bindSPE` (\x ->
+      putSPE "state" `bindSPE` (\_ ->
+      modifySPE (\s -> "modified state") `bindSPE` (\_ ->
+      errorSPE ("It just throws an error for some reason"))))
+
+t2 = (unPE (unSPE t "initial state")) (Pos 0 0)
+  where
+    t = (unitSPE 1) `bindSPE` (\x ->
+      putSPE "state" `bindSPE` (\_ ->
+      modifySPE (\s -> "modified state") `bindSPE` (\_ ->
+      unitSPE 10)))
+
+
+
+-- State specifics
+
+modifySPE :: (s -> s) -> SPE s ()
+modifySPE f = SPE $ \s -> unitPE ((), f s)
+
+putSPE :: s -> SPE s ()
+putSPE s' = SPE $ \s -> unitPE ((), s)
+
+getSPE :: SPE s s
+getSPE = SPE $ \s -> unitPE (s, s)
+
+-- lifting:
+-- error
+errorSPE a = SPE $ \s -> errorPE a
+
+-- reset
+-- resetSPE pos (SPE m) = SPE $ \s -> let (m', s') = m s
+--                                     in (resetPE pos m' , s)
+
+
+------------------------------------------------
+
+-- OSPE monad: Position + Error + State Transformer + Output
+
+newtype OSPE s a = OSPE { unOSPE :: SPE s (String, a) }
+
+unitOSPE a = OSPE . unitSPE $ ("", a)
+
+bindOSPE :: OSPE s a -> (a -> OSPE s b) -> OSPE s b
+m `bindOSPE` k = OSPE $
+  (unOSPE m) `bindSPE` (\(out, a) ->
+    unOSPE (k a) `bindSPE` (\(out1, b) ->
+      unitSPE (out ++ out1, b)))
+
+
+-- Output monde
+outOSPE a = (show a ++ "; ", unitSPE ())
+
+
+-- lifting, eh...
+-- State specifics
+modifyOSPE :: (s -> s) -> OSPE s ()
+modifyOSPE f = OSPE $
+  modifySPE f `bindSPE` (\_ ->
+  unitSPE ("", ()))
+
+
+putOSPE :: s -> OSPE s ()
+putOSPE s = OSPE $
+  putSPE s `bindSPE` (\_ ->
+  unitSPE ("", ()))
+
+
+getOSPE :: OSPE s s
+getOSPE = OSPE $
+  getSPE `bindSPE` (\s ->
+    unitSPE ("", s))
+
+
+-- more lifting:
+-- error
+errorOSPE a = OSPE $ errorSPE a
+
+-- reset
+resetOSPE pos (OSPE m) = OSPE $
+  undefined
+
+--   (o, resetSPE pos m)
 
 ------------------------------------------------
 
 type M = I
-
-bindM = bindI
 unitM = unitI
+bindM = bindI
+
+-- type M = OSPE Int
+
+-- bindM = bindOSPE
+-- unitM = unitOSPE
+
 
 
 type Name = String
@@ -84,6 +189,7 @@ data Term  = Var Name
            | Lam Name Term
            | App Term Term
            | At Position Term
+           | Out Term
 
 data Value = Wrong
            | Num Int
@@ -98,7 +204,9 @@ showval (Fun f) = "<function>"
 
 eval :: Term -> Env -> M Value
 eval (Var x) e = lookup' x e
+
 eval (Cont i) e = unitM (Num i)
+
 eval (Add u v) e = eval u e `bindM` \a ->
                    eval v e `bindM` \b ->
                    add a b
@@ -123,5 +231,16 @@ add _ _             = unitM Wrong
 apply :: Value -> Value -> I Value
 apply (Fun k) a = k a
 apply _ _       = unitM Wrong
+
+
+
+
+{-@ Monad Laws guarantees monad compose. @-}
+
+{-@ Continuation monad
+@-}
+
+
+{-@ Monad and CPS @-}
 
 
