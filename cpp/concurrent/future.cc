46bf5eaa6de1b48c5e38464ee50c54b31f52a87f
Jimmy <jimmy123good@hotmail.com>
2021-06-06 14:57:33 -0700

    async and promise:

--- cpp/concurrent/future.cc
+++ cpp/concurrent/future.cc
@@ -5,12 +5,71 @@
 #include <thread>
 #include <vector>
 
+// https://en.wikipedia.org/wiki/Futures_and_promises
+//
+// C++ async mechanism:
+// An async operation has two side: [the producer/wr] and [the consumer]
+//
+// - Why promise and future are separate?
+//   1. std::promise is a writable pending value (the function set the value),
+//      std::future is a readony pending value (the value).
+//
+//      future can be defiend without specifying which pormise to set its value.
+//      the value of a future can be set by any promise, but it can only be set once.
+//
+//      Settting the value of a future is called resolving/fulfilling.
+//
+// - What's the motivation of promise and future if we have thread already?
+//   promise based model makes concurrent code easier to write.
+//
+//   futures and promise decouple the computation and the return value of the computation,
+//   You can refer to the result of the computation as future and use freely without
+//   concern how the value is computeted.
+//
+//   If the value is computed, where the value is used can preceed. Otherwise the thread
+//   will wait for the value to arrive.
+//
+//   Doing so we can avoid writing too many low level locks. Essentially get put a lock
+//   on the value acces it iself.
+//
+// - What does it mean by having more flexibility on computation (decouple computation and result)?
+//   If you have a future, you can use it as other normal values.
+//
+//   Usually in an imperative setting, if you are using a value, it assumes some computation related
+//   to this value is done before you are using it. In another word, the existence of the value
+//   is part of the precondition of where you use it.
+//
+//   If you wrap the computation in a promise and use it's result as future, it's not necessarily
+//   true that the value is computed when you are using it. The related computation is in promise
+//   and is invoked when needed, a bit like lazy eval.
+//
+//   But because the whole computation is captured by the promise, it doesn't matter how you execute
+//   it. You can run the code in promise synchronously, asynchornously, in another thread, in anther
+//   core, or even somewhere far far away.
+//
+//   With promies and future, all you need to ensure is when future.get() is called, the computation
+//   is done somewhere and the valueeventually arives. The flexibility comes by the fact that the
+//   computation is decoupled from the current control flow.
+//
+// - Is it necessary for future and promise be two separate things?
+//   No, you can totally create a promise by specifying what computation should it run,
+//   and what value it should return, and use the exact same promise as the pending value.
+//   It's just happen C++ stl design promose like this.
+//   Now you not only have lower level primitives like promise + thread, you also can built
+//   async which is a higher level interface.
+//
+// - Is c++ async model CSP? (communitaion sequential process)
+//
+//
+
 // std::future provides mechanism to access the result of asynchronous
 // operations. What is an asynchronous operation?
 //   operations created by
 //    1. std::async
 //    2. std::packaged_task
 //    3. std::promise
+
+
 
 namespace promise_demo {
 //// std::promise
@@ -107,15 +166,15 @@ struct X {
     std::cout << str << " " << i << std::endl;
   }
 
-  void bar(const std::string *str) {
+  void bar(const std::string &str) {
     std::lock_guard<std::mutex> lk(*m);
     std::cout << str << std::endl;
   }
 
-  int operator()(int i) {
+  std::string operator()(int i, const std::string &str) {
     std::lock_guard<std::mutex> lk(*m);
-    std::cout << i << std::endl;
-    return i + 10;
+    std::cout << str << " " << i << std::endl;
+    return "DONE: " + str;
   }
 };
 
@@ -145,9 +204,33 @@ void async_demo() {
                            []() { std::cout << "I am a thread" << std::endl; });
   future.get(); // retrieve the value from future.
 
+  std::cout << "\n";
   {
+    // use async to run parallel...
     std::vector<int> v(10000, 2);
     std::cout << "Sum is: " << parallel_sum(v.begin(), v.end()) << std::endl;
+  }
+
+  std::cout << "\n";
+  {
+    std::shared_ptr<std::mutex> m = std::make_shared<std::mutex>();
+    X x{m};
+    // (&a)->foo(42, "hello");
+    auto a1 = std::async(&X::foo, &x, 42, "a1: hello, I maybe async or deferred");
+
+    // x.bar("world") in lazy
+    auto a2 =
+        std::async(std::launch::deferred, &X::bar, x, "a2: world!, I'm deferred");
+
+    // X()(43) in async
+    auto a3 =
+        std::async(std::launch::async, X{m}, 43, "a3: I'm in another thread!");
+    std::cout << a3.get() << std::endl;
+
+    //  cannot get twice
+    // std::cout << a3.get() << std::endl;
+
+    a2.wait(); // lazy eval
   }
 
   std::cout << "End future demo: ===" << std::endl;
