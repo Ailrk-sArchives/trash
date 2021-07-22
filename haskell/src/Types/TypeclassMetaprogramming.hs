6b3760fda433c4b087d9a18525159b658f235814
Jimmy <jimmy123good@hotmail.com>
2021-07-22 15:59:51 -0700

    even list

--- haskell/src/Types/TypeclassMetaprogramming.hs
+++ haskell/src/Types/TypeclassMetaprogramming.hs
@@ -6,6 +6,10 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications    #-}
 {-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 
 module Types.TypeclassMetaprogramming where
 import qualified Data.Map               as Map
@@ -14,11 +18,13 @@ import           Data.Void
 import           Numeric.Natural
 
 import           Control.Exception.Base (assert)
+import System.IO
 
 -- https://lexi-lambda.github.io/blog/2021/03/25/an-introduction-to-typeclass-metaprogramming/
 
 -------------------------------------------------------------------------------
 -- typeclass as function from type to term
+-- use compile time information to generate runtime behavior.
 
 class TypeOf a where
   typeOf :: String
@@ -41,16 +47,15 @@ instance TypeOf a => TypeOf [a] where
 
 -------------------------------------------------------------------------------
 -- type level interpreter
-data Z
-data S a
+data Nat = Z | S Nat
 
-class ReifyNat a where
+class ReifyNat (a :: Nat) where
   reifyNat :: Natural
 
-instance ReifyNat Z where
+instance ReifyNat 'Z where
   reifyNat = 0
 
-instance ReifyNat a => ReifyNat (S a) where
+instance ReifyNat a => ReifyNat ('S a) where
   reifyNat = 1 + (reifyNat @a)
 
 -------------------------------------------------------------------------------
@@ -108,7 +113,6 @@ instance (ElementOf [a] ~ a) => Flatten [a] where
 instance {-# OVERLAPPING #-} Flatten [a] => Flatten [[a]] where
   flatten = flatten . concat
 
-
 n2 = flatten [[[1 :: Integer, 2], [3, 4]], [[5, 6], [7, 8]]]
 
 -------------------------------------------------------------------------------
@@ -147,6 +151,9 @@ data Authentication = AuthBaisc Username Password
 class GNumFields a where
   gnumFields :: a -> Natural
 
+instance {-# OVERLAPPING #-} GNumFields () where
+  gnumFields _ = 0
+
 instance GNumFields a where
   gnumFields _ = 1
 
@@ -157,34 +164,143 @@ instance {-# OVERLAPPING #-} (GNumFields a, GNumFields b) => GNumFields (Either 
   gnumFields (Left a)  = gnumFields a
   gnumFields (Right b) = gnumFields b
 
-authToEither :: Authentication -> Either (Username, Password) PublicKey
-authToEither (AuthBaisc u p) = Left (u, p)
-authToEither (AuthSSH p)     = Right p
+authGenerialize :: Authentication -> Either (Username, Password) PublicKey
+authGenerialize (AuthBaisc u p) = Left (u, p)
+authGenerialize (AuthSSH p)     = Right p
 
 numFieldsAuth :: Authentication -> Natural
-numFieldsAuth = gnumFields . authToEither
+numFieldsAuth = gnumFields . authGenerialize
 
 n4 = numFieldsAuth (AuthSSH "asd")
 n5 = numFieldsAuth (AuthBaisc "asd" "asd")
 
 -------------------------------------------------------------------------------
 -- Define a generic  NumFields that works on any types.
+-- The idea is to convert a type to a isomorphic representation that we can
+-- work with.
+-- If we have typeclass that works on the transformed representation, we can
+-- do various things with the new representation and map it back when we
+-- are finished
 
-class Generik a where
-  type Rep a
-  genericize :: a -> Rep a
+class Generik1 a where
+  type Rep1 a
+  genericize1 :: a -> Rep1 a
 
-instance Generik Authentication where
-  type Rep Authentication = Either (Username, Password) PublicKey
-  genericize (AuthBaisc user pass) = Left (user, pass)
-  genericize (AuthSSH key)         = Right key
+-- Rep maps the type to it's representation type.
+-- in our case we want to map it to either or tuple so we can count with GNumFields.
+instance Generik1 Authentication where
+  type Rep1 Authentication = Either (Username, Password) PublicKey
+  genericize1 (AuthBaisc user pass) = Left (user, pass)
+  genericize1 (AuthSSH key)         = Right key
 
-numFields :: (Generik a, Generik (Rep a)) => a -> Natural
-numFields = gnumFields . genericize
+-- we want (Rep a) available for GNumFields
+numFields :: (Generik1 a, GNumFields (Rep1 a)) => a -> Natural
+numFields = gnumFields . genericize1
 
-
+n6 = numFields (AuthBaisc "asd" "asd")
+n7 = numFields (AuthSSH "asd")
 
 -------------------------------------------------------------------------------
+-- improve generic
+-- 1. this type will give wrong numfields because Rep a will be two nested
+--    either, one is only a representation for the sum type.
+--    To solve this, we can wrap each leaf with a new type Leaf.
+data Foo = A (Either Int String) | B (Char, Bool)
+data Boool = FF | TT
+newtype Leaf a = Leaf { getLeaf :: a }
+
+class Generik2 a where
+  type Rep2 a
+  genericize2 :: a -> Rep2 a
+
+instance Generik2 Authentication where
+  type Rep2 Authentication = Either (Leaf Username, Leaf Password) (Leaf PublicKey)
+  genericize2 (AuthBaisc user pass) = Left (Leaf user, Leaf pass)
+  genericize2 (AuthSSH key) = Right (Leaf key)
+
+instance Generik2 Foo where
+  type Rep2 Foo = Either (Leaf (Either Int String)) (Leaf (Char, Bool))
+  genericize2 (A x) = Left (Leaf x)
+  genericize2 (B x) = Right (Leaf x)
+
+instance Generik2 Boool where
+  type Rep2 Boool = Either () ()
+  genericize2 FF = Left ()
+  genericize2 TT = Right ()
+
+numFields2 :: Generik2 a => GNumFields (Rep2 a) => a -> Natural
+numFields2 = gnumFields . genericize2
+
+n8 = numFields2 (A (Left 1))
+n9 = numFields2 (B ('a', True))
+n10 = numFields2 (AuthBaisc "asd" "asd")
+n11 = numFields2 TT
+
+------------------------------------------------------------------------------
+-- dependent types
+
+-- Note: a has kind *, so it will accpet whatever types. If the pattern
+-- matching fails, it will get stuck.
+-- For example, BadNot Char will return BadNot Char instead of evaluate further
+--
+-- DataKind promotes types to new kinds and constructors to types, which give
+-- us more elements to work with at the type level.
+type family BadNot a where
+  BadNot 'True = 'False
+  BadNot 'False = 'True
+
+type family Not (a :: Bool) :: Bool where
+  Not 'True = 'False
+  Not 'False = 'True
+
+------------------------------------------------------------------------------
+-- runtime information -> compile time type
+-- GADT and proof terms
+
+data WhatIsIt a where
+  ABool :: WhatIsIt Bool
+  AInt :: WhatIsIt Int
+
+-- the type of x is determined by tye constructor.
+-- which constructor is passed is a runtime behavior. But we can infer the type
+-- of the type parameter with GDAT
+--
+-- Think GADT as a proof of type equalities.
+-- ABool => a ~ Bool
+-- AInt => a ~ Int
+doSometing :: WhatIsIt a -> a -> a
+doSometing ABool x = not x
+doSometing AInt x = x + 1
+
+-- GADT with type level list.
+infixr 5 `HCons`
+
+-- hetergenous list
+data HList as where
+  HNil :: HList '[]
+  HCons :: a -> HList as -> HList (a ': as)
+
+n12 = True `HCons` "hellow" `HCons` 42 `HCons` HNil
+
+------------------------------------------------------------------------------
+-- proofs that works together.
+data OneToThree a b c as where
+  One :: OneToThree a b c '[a]
+  Two :: OneToThree a b c '[a, b]
+  Three :: OneToThree a b c '[a, b, c]
+
+sumUpToThree :: OneToThree Int Int Int as -> HList as -> Int
+sumUpToThree One (x `HCons` HNil) = x
+sumUpToThree Two (x `HCons` y `HCons` HNil) = x + y
+sumUpToThree Three (x `HCons` y `HCons` z `HCons` HNil) = x + y + z
+
+------------------------------------------------------------------------------
+data Even as where
+  EvenNil :: Even '[]
+  EvenCons :: Even as -> Even (a ': b ': as)
+
+
+------------------------------------------------------------------------------
 run :: IO ()
 run = do
   assert (n1 == 5) (return ())
@@ -192,3 +308,13 @@ run = do
   assert (n3 == [1..4]) (return ())
   assert (n4 == 1) (return ())
   assert (n5 == 2) (return ())
+  assert (n6 == 2) (return ())
+  assert (n7 == 1) (return ())
+  assert (n8 == 1) (return ())
+  assert (n9 == 1) (return ())
+  assert (n10 == 2) (return ())
+  assert (n11 == 0) (return ())
+
+xproc0 = openFile "" ReadMode
+xproc1 = openFile "" ReadMode
+xproc2 = openFile "" ReadMode
